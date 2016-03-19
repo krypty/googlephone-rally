6a8c058fea30525fb518275df48a380ac988d2ab
Gary Marigliano <gmarigliano93@gmail.com>
2016-03-19 16:28:32 +0100

    First commit

---
+++ app/src/main/java/ch/hes_so/master/phonerally/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.hes_so.master.phonerally;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
