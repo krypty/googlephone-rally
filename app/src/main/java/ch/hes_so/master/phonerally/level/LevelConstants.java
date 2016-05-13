94f1a1574f82e428cd80e7ee6e797ec95c2ed72f
Gary Marigliano <gmarigliano93@gmail.com>
2016-05-13 16:23:57 +0200

    Fix level count

--- app/src/main/java/ch/hes_so/master/phonerally/level/LevelConstants.java
+++ app/src/main/java/ch/hes_so/master/phonerally/level/LevelConstants.java
@@ -2,6 +2,6 @@ package ch.hes_so.master.phonerally.level;
 
 
 public class LevelConstants {
-    public static final int MAX_LEVEL = 3;
+    public static final int MAX_LEVEL = 5;
     public static final String LEVEL_PREFIX = "level";
 }
